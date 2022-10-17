1db5ef9194cf31a78b8e455e88f9e7900da46fca
renatorochaal <renatturocha2@hotmail.com>
2022-10-17 09:01:09 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/Curso.java
@@ -0,0 +1,34 @@
+package br.com.dio.desafio;
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+}
